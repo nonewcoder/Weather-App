edf0bdb484265ae95a670f3993d10d040dd5b237
Anzal <mohdanzalansari@gmail.com>
2021-12-11 18:32:24 +0530

    Weather App

---
+++ custom.js
@@ -0,0 +1,55 @@
+window.addEventListener("load",()=>{
+
+    let long;
+    let lat;
+    let query;
+    let celsiusSet=true;
+
+    if (navigator.geolocation){
+
+        navigator.geolocation.getCurrentPosition(position=>
+            {
+                lat=position.coords.latitude;
+                lang= position.coords.longitude;
+
+                query=`http://api.weatherapi.com/v1/current.json?key=53b8bd0a6df2473f82b112428211112&q=${lat},${lang}&aqi=no`;
+
+
+                fetch(query)
+                .then(response=>response.json())
+                .then(data=>{
+                    
+                    document.getElementById("location").innerText=data.location.name;
+                    document.getElementById("image").innerHTML=`<img src=${data.current.condition.icon}>`;
+                    document.getElementById("temperature").innerText=data.current.temp_c+"° C";
+                    document.getElementById("comment").innerText=data.current.condition.text;
+
+                    document.getElementById("temperature").addEventListener('click',()=>{
+                        if (celsiusSet){
+                        document.getElementById("temperature").innerText=data.current.temp_f+"° F";
+                        celsiusSet=false;
+                        }
+                        else{
+                            document.getElementById("temperature").innerText=data.current.temp_c+"° C";
+                            celsiusSet=true;
+                        }
+                    })
+
+});
+
+
+
+            });
+            
+
+    }
+
+    
+
+});
+document.getElementById("image").addEventListener('click',change);
+
+function change()
+{
+    document.getElementById("image").innerHTML="<img src='//cdn.weatherapi.com/weather/64x64/day/113.png'>"
+}
